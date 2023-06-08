2faeb1ac6a69d7dfd4ccdf12fa161da0863ad9d0
Mateo Carrizo FLores <127887470+zErhiz@users.noreply.github.com>
2023-06-08 10:51:25 -0300

    fix2d (#3)

--- challenge/src/navigation/BottomTabsNav.js
+++ challenge/src/navigation/BottomTabsNav.js
@@ -3,7 +3,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/Home";
 import Cart from "../screens/Cart";
 import Register from "../screens/Register";
+import ExploreProducs from "../screens/ExploreProducts";
 import SignIn from "../screens/SignIn";
+import AllProducts from "../screens/AllProducts";
 const Tab = createBottomTabNavigator();
 
 function BottomTabsNavigator() {
@@ -13,7 +15,8 @@ function BottomTabsNavigator() {
       <Tab.Screen name="Register" component={Register} />
       <Tab.Screen name="SignIn" component={SignIn} />
       <Tab.Screen name="Cart" component={Cart} />
-
+      <Tab.Screen name="ExploreProducts" component={ExploreProducs} />
+      <Tab.Screen name="AllProducts" component={AllProducts} />
     </Tab.Navigator>
   );
 }
---
+++ challenge/src/screens/AllProducts.jsx
@@ -0,0 +1,17 @@
+import React, { useRef, useState, useEffect } from "react";
+import { View, Text, TextInput, TouchableOpacity, ScrollView, ImageBackground, Image } from "react-native";
+import apiUrl from "../../api";
+
+const AllProducts = () => {
+  return (
+    <View>
+        <Text>
+            AllProducts 
+
+        </Text>
+        
+    </View>
+  )
+}
+
+export default AllProducts
