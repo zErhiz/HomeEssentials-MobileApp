73ba018a9074d5e78366a5be273f72df20f370c1
Mateo Carrizo Flores <mateocarrizoflores@gmail.com>
2023-06-15 13:14:10 -0300

    fix

--- challenge/src/navigation/BottomTabsNav.js
+++ challenge/src/navigation/BottomTabsNav.js
@@ -13,6 +13,7 @@ import SearchBar from "../screens/SearchBar"
 import CategoryProduct from "../screens/CategoryProducts"
 import logo from '../../assets/Logos/logo-2-b.png'
 import Profile from "../screens/Profile";
+
 const Tab = createBottomTabNavigator();
 
 function BottomTabsNavigator() {
@@ -64,23 +65,6 @@ function BottomTabsNavigator() {
         </View>,
       })}
       />
-  
-   
-      
-      <Tab.Screen name="Category" component={CategoryProduct} options={() => ({
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="wpexplorer" color="purple" size={size} />
-          ),
-        headerTitle: () => <View style={{ width: 300, display: "flex", justifyContent: "space-between", alignItems: "center", flexDirection: "row" }}>
-          <Image source={logo} style={{
-            height: 25, width: 100
-          }} />
-          <TouchableOpacity onPress={() => navigation.navigate("SearchInput")}>
-            <FontAwesome name="search" size={16} color="#7847E0" />
-          </TouchableOpacity>
-        </View>,
-      })}
-      />
         
         <Tab.Screen name="Profile" component={Profile} options={() => ({
           tabBarIcon: ({ color, size }) => (
@@ -96,6 +80,8 @@ function BottomTabsNavigator() {
         </View>,
       })}
       />
+       
+
     </Tab.Navigator>
   );
 }
